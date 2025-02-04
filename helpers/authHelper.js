09cce3d096a863e1f67423a6aa00db8693ae2c5c
ankan-coder <76089611+ankan-coder@users.noreply.github.com>
2025-02-04 12:13:18 +0530

    bcrypt error solving

--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -1,9 +1,10 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcryptjs");
 
 // Hash Function
 exports.hashPassword = (password) => {
   return new Promise((resolve, reject) => {
-    bcrypt.genSalt(10, (err, salt) => { // Corrected the syntax here
+    bcrypt.genSalt(10, (err, salt) => {
+      // Corrected the syntax here
       if (err) {
         reject(err);
       }
